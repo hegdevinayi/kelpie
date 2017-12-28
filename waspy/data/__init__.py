bee6b8c9910410d12d94d1d7c918f1470c37b0c9
Vinay Hegde <hegdevinayi@gmail.com>
2017-12-27 21:59:35 -0600

    Global variable STD_ATOMIC_WEIGHTS defined.

---
+++ waspy/data/__init__.py
@@ -0,0 +1,15 @@
+import json
+import os
+
+
+# GLOBAL data
+
+# Standard atomic weights
+# Source: Meija et al., "Atomic weights of the elements 2013 (IUPAC Tehcnical Report)", Pure Appl. Chem. 88, 265 (2016)
+# For elements missing from the above list, source:
+# Sansonetti et al., Handbook of Basic Atomic Spectroscopic Data (version 1.1.3). NIST, Gaithersburg, MD
+# Accessed from http://physics.nist.gov/Handbook [Date: Dec 27, 2017]
+atomic_weights_file = os.path.join('data', 'standard_atomic_weights.json')
+with open(atomic_weights_file, 'r') as fr:
+    STD_ATOMIC_WEIGHTS = json.load(fr)
+
