cac0ad7d4f7ee8a131f4c79dc021ddff1326a38d
Vinay Hegde <hegdevinayi@gmail.com>
2017-12-29 14:48:45 -0600

    Refactor `VaspInputGenerator` as per previous edits
    - `structure` can now be a `Structure` object or path to a POSCAR file
    - Move setting ENCUT, scaling EDIFF into `calculation_settings.setter`
    - `write_location` now defaults to `Structure.structural_formula`
    - `get_vasp_potcar()`, `get_vasp_incar()` now protected
    - Update defaults for `encut_scaling_factor`, `maximum_encut` (1.5 and
    600 respectively)
    - Include "overwrite" switch for `write_vasp_input_files()`
    - Remove messages to stdout. Interaction with the user only if
    `overwrite` is set to False *and* file(s) exist(s) in `write_location`

--- waspy/vasp_input_generator.py
+++ waspy/vasp_input_generator.py
@@ -4,8 +4,7 @@ import math
 import waspy
 from waspy import io
 from waspy.structure import Structure, StructureError
-from waspy.structure import _check_structure_is_complete
-from waspy.vasp_settings.incar import VASP_INCAR_TAGS
+from waspy.vasp_settings.incar import DEFAULT_VASP_INCAR_SETTINGS, VASP_INCAR_TAGS
 from waspy.vasp_settings.potcar import VASP_RECO_POTCARS
 
 
@@ -20,15 +19,15 @@ class VaspInputGenerator(object):
     def __init__(self,
                  structure=None,
                  calculation_settings=None,
-                 write_location=None):
+                 write_location=None,
+                 **kwargs):
         """
-        :param structure: `waspy.structure.Structure` object.
-        :type structure: `waspy.vasp_structure.VaspStructure`
-        :param calculation_settings: VASP INCAR tags, values, and POTCAR choices.
-        :type calculation_settings: dict(str, str or float or bool or list or dict)
-        :param write_location: where the VASP input files should be written.
-                               Defaults to the location of the `vasp_structure.structure_file`.
-        :type write_location: str
+        :param structure: `waspy.structure.Structure` object or String with the location of a valid VASP5 POSCAR file
+        :param calculation_settings: Dictionary of VASP INCAR tags, values, and POTCAR choices.
+                                     Defaults to pre-defined full relaxation calculation.
+        :param write_location: String with the location where the VASP input files should be written.
+                               Defaults to folder `structure.structural_formula` in the current working directory
+        :param kwargs: Dictionary of other parameters, if any
         """
 
         #: `waspy.structure.VaspStructure` object containing the VASP POSCAR data
@@ -36,17 +35,12 @@ class VaspInputGenerator(object):
         self.structure = structure
 
         #: VASP INCAR tags, values and POTCAR choices for generating input files.
+        self._calculation_settings = None
         self.calculation_settings = calculation_settings
 
         #: where the VASP input files should be written
-        if write_location is None:
-            self.write_location = os.path.dirname(self.vasp_structure.poscar_file)
-        else:
-            self.write_location = write_location
-
-        # if ENCUT is not set in calculation settings, set it manually
-        if self.calculation_settings.get('encut') is None:
-            self.set_calculation_encut()
+        self._write_location = None
+        self.write_location = write_location
 
     @property
     def structure(self):
@@ -55,9 +49,47 @@ class VaspInputGenerator(object):
     @structure.setter
     def structure(self, structure):
         if isinstance(structure, Structure):
-            _check_structure_is_complete(structure)
+            structure.check_structure_is_complete()
             self._structure = structure
-        elif isinstance(structure, str)
+        elif isinstance(structure, str):
+            if os.path.isfile(structure):
+                structure = io.read_poscar(poscar_file=structure)
+            self._structure = structure
+        else:
+            error_message = '`structure` must be `structure.Structure` object or path to a valid POSCAR'
+            raise StructureError(error_message)
+
+    @property
+    def calculation_settings(self):
+        return self._calculation_settings
+
+    @calculation_settings.setter
+    def calculation_settings(self, calculation_settings):
+        if not calculation_settings:
+            self._calculation_settings = DEFAULT_VASP_INCAR_SETTINGS['relaxation']
+        else:
+            self._calculation_settings = calculation_settings
+
+        # if ENCUT is not set in calculation settings, set it manually
+        if self._calculation_settings.get('encut') is None:
+            self.set_calculation_encut()
+
+        # Scale EDIFF with the number of atoms in the unit cell
+        if self._calculation_settings.get('ediff_per_atom'):
+            if self._calculation_settings.get('ediff') is None:
+                self._calculation_settings.update({'ediff': DEFAULT_VASP_INCAR_SETTINGS['relaxation']['ediff']})
+            self._calculation_settings.update({'ediff': self._calculation_settings['ediff']*self.structure.natoms})
+
+    @property
+    def write_location(self):
+        return self._write_location
+
+    @write_location.setter
+    def write_location(self, write_location):
+        if not write_location:
+            self._write_location = self.structure.structural_formula
+        else:
+            self.write_location = write_location
 
     def vasp_tag_value_formatter(self, value):
         if isinstance(value, list):
@@ -106,9 +138,9 @@ class VaspInputGenerator(object):
         :return: contents of the VASP POTCAR file.
         :rtype: str
         """
-        return self.get_vasp_potcar()
+        return self._get_vasp_potcar()
 
-    def get_vasp_potcar(self):
+    def _get_vasp_potcar(self):
         """Construct the VASP POTCAR for `self.POSCAR`.
 
         If `self.calculation_settings['potcar_settings']['element_potcars'] dictionary:
@@ -122,20 +154,22 @@ class VaspInputGenerator(object):
         :raise VaspInputError: if POTCAR file for any element is not found.
         """
         potcar_settings = self.calculation_settings['potcar_settings']
+        version = potcar_settings['version']
+        xc = potcar_settings['xc']
         element_potcars = potcar_settings.get('element_potcars', {})
 
         list_of_potcars = []
-        for element in self.vasp_structure.list_of_elements:
-            element_potcar = element_potcars.get(element)
+        for species in self.structure.list_of_species:
+            element_potcar = element_potcars.get(species)
 
             # if POTCAR choices are not specified in the calculation settings, use defaults.
             if element_potcar is None:
-                potcar_dir = VASP_RECO_POTCARS[potcar_settings['version']][potcar_settings['xc']]['path']
-                reco_pots = VASP_RECO_POTCARS[potcar_settings['version']][potcar_settings['xc']]['reco']
+                potcar_dir = VASP_RECO_POTCARS[version][xc]['path']
+                reco_pots = VASP_RECO_POTCARS[version][xc]['reco']
                 try:
-                    potcar_file = os.path.join(potcar_dir, reco_pots[element], 'POTCAR')
+                    potcar_file = os.path.join(potcar_dir, reco_pots[species], 'POTCAR')
                 except KeyError:
-                    error_message = 'POTCAR for {} not found.'.format(element)
+                    error_message = 'POTCAR for {} not found'.format(species)
                     raise VaspInputError(error_message)
                 else:
                     list_of_potcars.append(potcar_file)
@@ -146,12 +180,12 @@ class VaspInputGenerator(object):
 
             # if VASP POTCAR keys (e.g. "Li_sv", "Mn_pv") are specified, read them from the local POTCAR library.
             else:
-                potcar_dir = VASP_RECO_POTCARS[potcar_settings['version']][potcar_settings['xc']]['path']
+                potcar_dir = VASP_RECO_POTCARS[version][xc]['path']
                 potcar_file = os.path.join(potcar_dir, element_potcar, 'POTCAR')
                 if os.path.isfile(potcar_file):
                     list_of_potcars.append(potcar_file)
                 else:
-                    error_message = '{} file not found.'.format(potcar_file)
+                    error_message = 'POTCAR file {} for {} not found'.format(potcar_file, species)
                     raise VaspInputError(error_message)
         return self._concatenate_potcars(list_of_potcars)
 
@@ -183,15 +217,15 @@ class VaspInputGenerator(object):
     def set_calculation_encut(self):
         """Update `self.calculation_settings['encut']` = encut_scaling_factor*(highest ENMAX from `self.POTCAR`).
 
-        If `encut_scaling_factor` is not specified in calculation settings, 1.3 is used as default.
-        If `maximum_encut` is not specified in calculation settings, 520 (in eV) is used as default.
+        If `encut_scaling_factor` is not specified in calculation settings, 1.5 is used as default.
+        If `maximum_encut` is not specified in calculation settings, 600 (in eV) is used as default.
         """
-        encut_scaling_factor = self.calculation_settings.get('encut_scaling_factor', 1.3)
-        maximum_encut = self.calculation_settings.get('maximum_encut', 520)
+        encut_scaling_factor = self.calculation_settings.get('encut_scaling_factor', 1.5)
+        maximum_encut = self.calculation_settings.get('maximum_encut', 600)
         encut = self.roundup_encut(encut_scaling_factor*self.get_highest_enmax(self.POTCAR))
         if encut > maximum_encut:
             encut = maximum_encut
-        self.calculation_settings.update({'encut': encut})
+        self._calculation_settings.update({'encut': encut})
 
     @property
     def INCAR(self):
@@ -200,9 +234,9 @@ class VaspInputGenerator(object):
         :return: contents of the VASP INCAR file.
         :rtype: str
         """
-        return self.get_vasp_incar()
+        return self._get_vasp_incar()
 
-    def get_vasp_incar(self):
+    def _get_vasp_incar(self):
         """Construct the VASP INCAR file for `self.POSCAR` using `self.calculation_settings`
 
         :return: VASP INCAR file
@@ -221,50 +255,48 @@ class VaspInputGenerator(object):
         return vasp_incar
 
     @staticmethod
-    def _write_vasp_input_file(file_contents, file_location):
+    def _write_vasp_input_file(file_contents, file_location, overwrite=True):
         """Write the `file_contents` as is into `file_location`; confirm overwrite if file already exists.
 
         :param file_contents: contents to be written into file
         :type file_contents: str
         :param file_location: absolute or relative path to the file
         :type file_location: str
+        :param overwrite: if `file_location` already exists, should it be overwritten?
         """
-        if os.path.isfile(file_location):
-            sys.stdout.write('File {} already exists.'.format(file_location))
-            user_input = input('Overwrite? [y/n]: ')
-            while user_input.lower() not in ['y', 'n']:
-                user_input = input('Type "y" or "n": ')
-            if user_input.lower() == 'n':
-                sys.stdout.write('OK, skipped writing {}.\n'.format(file_location))
-                sys.stdout.flush()
-                return
+        if not overwrite:
+            if os.path.isfile(file_location):
+                sys.stdout.write('File {} already exists.'.format(file_location))
+                user_input = input('Overwrite? [y/n]: ')
+                while user_input.lower() not in ['y', 'n']:
+                    user_input = input('Type "y" or "n": ')
+                if user_input.lower() == 'n':
+                    sys.stdout.write('OK, skipped writing {}.\n'.format(file_location))
+                    sys.stdout.flush()
+                    return
 
         with open(file_location, 'w') as fw:
             fw.write(file_contents)
         return
 
-    def write_vasp_input_files(self):
-        """Write VASP POSCAR, POTCAR, and INCAR files into the folder specified by `location`."""
+    def write_vasp_input_files(self, overwrite=True):
+        """Write VASP POSCAR, POTCAR, and INCAR files into the folder specified by `self.write_location`.
+
+        :param overwrite: Should files be overwritten if they already exist? Defaults to True."""
         try:
             os.mkdir(self.write_location)
         except FileExistsError:
             pass
 
-        sys.stdout.write('Writing VASP input files into "{}"... '.format(self.write_location))
-        sys.stdout.flush()
-
         # write the POSCAR file
         poscar_file = os.path.join(self.write_location, 'POSCAR')
-        self._write_vasp_input_file(self.vasp_structure.POSCAR, poscar_file)
+        self._write_vasp_input_file(self.structure.POSCAR, poscar_file, overwrite=overwrite)
 
         # write the POTCAR file
         potcar_file = os.path.join(self.write_location, 'POTCAR')
-        self._write_vasp_input_file(self.POTCAR, potcar_file)
+        self._write_vasp_input_file(self.POTCAR, potcar_file, overwrite=overwrite)
 
         # write the INCAR file
         incar_file = os.path.join(self.write_location, 'INCAR')
-        self._write_vasp_input_file(self.INCAR, incar_file)
-
-        sys.stdout.write('done.\n')
-        sys.stdout.flush()
+        self._write_vasp_input_file(self.INCAR, incar_file, overwrite=overwrite)
 
