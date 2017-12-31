e2e1f26fb7411db911be6a850610c77d6894313a
Vinay Hegde <hegdevinayi@gmail.com>
2017-12-30 20:04:03 -0600

    Add unit tests (skeleton) for `vasp_run_manager`

---
+++ tests/test_vasp_run_manager.py
@@ -0,0 +1,54 @@
+import unittest
+from waspy.vasp_run_manager import VaspSingleRunManager
+
+
+def __get_vas
+
+class TestVaspSingleRunManager(unittest.TestCase):
+    """Base class to test `vasp_run_manager.VaspSingleRunManager`"""
+
+    def test_structure_file_not_specified(self):
+        pass
+
+    def test_structure(self):
+        pass
+
+    def test_calculation_workflow_not_specified(self):
+        pass
+
+    def test_calculation_workflow_not_recognized(self):
+        pass
+
+    def test_run_location_not_specified(self):
+        pass
+
+    def test_host_scheduler_settings_not_specified(self):
+        pass
+
+    def test_host_scheduler_settings_path_to_file(self):
+        pass
+
+    def test_host_scheduler_settings_nondefault_tag(self):
+        pass
+
+    def test_host_scheduler_settings_tag_not_recognized(self):
+        pass
+
+    def test_batch_script_template_not_specified(self):
+        pass
+
+    def test_batch_script_template_path_to_file(self):
+        pass
+
+    def test_batch_script_template_nondefault_tag(self):
+        pass
+
+    def test_batch_script_template_tag_not_recognized(self):
+        pass
+
+    def test_batch_script(self):
+        pass
+
+
+
+
