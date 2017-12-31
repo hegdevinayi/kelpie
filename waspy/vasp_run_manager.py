ddaee48796769a26c7c60a9b2983760b34184f9d
Vinay Hegde <hegdevinayi@gmail.com>
2017-12-30 20:03:08 -0600

    Refactor `vasp_run_manager`. Complete constructor.

--- waspy/vasp_run_manager.py
+++ waspy/vasp_run_manager.py
@@ -1,76 +1,198 @@
 import os
-from waspy import structure
+import json
+from waspy import io
 from waspy.vasp_input_generator import VaspInputGenerator
 from waspy.vasp_output_parser import VasprunXMLParser
+from waspy.scheduler_settings import DEFAULT_SCHEDULER_SETTINGS
+from waspy.scheduler_templates import SCHEDULER_TEMPLATES
 from waspy.vasp_settings.incar import DEFAULT_VASP_INCAR_SETTINGS
-from waspy.batch_scheduler_settings import DEFAULT_BATCH_SCHEDULER_SETTINGS
 
 
-class VaspRunManager(object):
+class VaspRunManagerError(Exception):
+    """Base class to handle errors associated with the run manager"""
+    pass
+
+
+class VaspSingleRunManager(object):
     """Base class to manage VASP runs."""
 
     def __init__(self,
-                 structure_file='POSCAR',
-                 calculation_workflow='relaxation',
-                 nondefault_calc_settings=None,
-                 run_directory=None,
-                 host_resource='cori',
-                 batch_scheduler='slurm',
-                 nondefault_scheduler_settings=None):
+                 structure_file=None,
+                 calculation_workflow=None,
+                 custom_calculation_settings=None,
+                 run_location=None,
+                 host_scheduler_settings=None,
+                 custom_scheduler_settings=None,
+                 batch_script_template=None,
+                 **kwargs):
         """
-        :param structure_file: location of the VASP POSCAR. Defaults to 'POSCAR'.
-        :type structure_file: str
-        :param calculation_workflow: type of DFT calculation (relaxation/static/hse/...). Defaults to 'relaxation'.
-        :type calculation_workflow: str
-        :param nondefault_calc_settings: nondefault INCAR, POTCAR settings for each calculation type. Defaults to None.
-                                         - e.g. {"relaxation": {"ediff": 1E-8, "nsw": 80}, "static": {"sigma": 0.1}}
-        :type nondefault_calc_settings: dict(str, dict(str, str or float or bool or list))
-        :param run_directory: directory to run all the calculations in. Defaults to the location of the `structure_file`.
-        :type run_directory: str
-        :param batch_scheduler: slurm/PBS/... Defaults to "slurm". (Only slurm implemented so far.)
-        :type batch_scheduler:
-        :param nondefault_scheduler_settings: tag, values to go into the batch script. Tags:
-                                   - job-name, partition, qos, account, constraint, license, nodes, time, output,
-                                   - omp_threads, module (to load), executable,
-                                   - n_mpi_per_node (# MPI tasks per node), n_cpu_bind (# CPUs to bind to an MPI task)
-        :type nondefault_scheduler_settings: dict(str, str)
-        :param host_resource: cori/edison/... Defaults to 'cori'.
-        :type host_resource: str
+        :param structure_file: String with the location of the VASP5 POSCAR file.
+                               (Default: './POSCAR')
+        :param calculation_workflow: String with type of DFT calculation (relaxation/static/hse/...).
+                                     (Default: "relaxation")
+        :param custom_calculation_settings: Dictionary of *nondefault* INCAR, POTCAR settings for each calculation type.
+                                            (The default settings dictionary will be updated)
+                                            - e.g. {"relaxation": {"ediff": 1E-8, "nsw": 80}, "static": {"sigma": 0.1}}
+                                            (Default: None)
+        :param run_location: String with the location where VASP calculations should be performed.
+                             (Default: location of the specified `structure_file`)
+        :param host_scheduler_settings: String with the name of a predefined host or path to a JSON file with the
+                                        scheduler settings. Must be one of the hosts defined in
+                                        `waspy.scheduler_settings` (with a corresponding
+                                        "[host_scheduler_tag].json" file) OR path to a JSON file with settings.
+                                        Path to a JSON file takes precedence.
+                                        E.g. "cori_knl", "cori_haswell" (predefined) OR "/path/to/settings.json"
+                                        (Default: "cori_knl")
+        :param custom_scheduler_settings: Dictionary of tags, and *nondefault* values to go into the batch script.
+                                          (The default settings dictionary, loaded for the specified
+                                          `host_scheduler_settings` above will be updated)
+                                          Possible tags:
+                                          - job-name
+                                          - partition
+                                          - qos
+                                          - account
+                                          - constraint
+                                          - license
+                                          - nodes
+                                          - walltime
+                                          - output
+                                          - omp_threads
+                                          - modules (to load)
+                                          - executable
+                                          - n_mpi_per_node (# MPI tasks per node)
+                                          - n_cpu_bind (# CPUs to bind to an MPI task)
+        :param batch_script_template: String with the path to a batch script template file OR the name of a
+                                      predefined template in the "scheduler_settings" directory. Path to a file takes
+                                      precedence.
+                                      (Default: "cori.q")
+        :param kwargs: Dictionary of other miscellaneous parameters, if any.
         """
 
         #: VASP POSCAR file containing the structure (only VASP 5 format currently supported).
-        self.structure_file = os.path.abspath(structure_file)
+        self._structure_file = None
+        self.structure_file = structure_file
+        #: `waspy.structure.Structure` object containing VASP POSCAR data.
+        self._structure = None
 
-        #: `waspy.vasp_structure.VaspStructure` object containing VASP POSCAR data.
-        self.vasp_structure = structure.VaspStructure(poscar_file=self.structure_file)
-
-        #: type of DFT calculation: relaxation/static/hse/...
-        if calculation_workflow not in ['relaxation', 'static']:
-            raise NotImplementedError('Only relaxation and static workflows currently implemented.')
+        #: Type of DFT calculation workflow: relaxation/static/hse/...
+        self._calculation_workflow = None
         self.calculation_workflow = calculation_workflow
 
-        #: nondefault INCAR settings and POTCAR choices for different calculation types
-        #: default INCAR, POTCAR settings defined by `waspy.vasp_settings.incar.DEFAULT_VASP_INCAR_SETTINGS`.
+        #: Nondefault INCAR settings and POTCAR choices for different calculation types
+        #: default INCAR, POTCAR settings defined by `waspy.vasp_settings.incar.DEFAULT_VASP_INCAR_SETTINGS` for the
+        # calculation workflow specified.
         #: VASP recommended POTCARs used by default are in `waspy.vasp_settings.potcar.VASP_RECO_POTCARS`.
-        self.nondefault_calc_settings = nondefault_calc_settings
+        self.custom_calculation_settings = custom_calculation_settings
 
-        #: relative/absolute path to run VASP calculations.
-        if run_directory is None:
-            self.run_location = os.path.dirname(self.structure_file)
+        #: Relative/absolute path to run VASP calculations.
+        self._run_location = None
+        self.run_location = run_location
+
+        #: Dictionary of batch scheduler settings corresponding to the host where the calculations are being run. Can
+        #  be path a JSON file with the settings OR one of the predefined tags in
+        # `waspy.scheduler_settings.DEFAULT_SCHEDULER_SETTINGS`. File takes precedence.
+        self._host_scheduler_settings = None
+        self.host_scheduler_settings = host_scheduler_settings
+
+        #: Nondefault scheduler settings for this particular run. Updates `self.host_scheduler_settings`.
+        self.custom_scheduler_settings = custom_scheduler_settings
+
+        #: Template for the batch script
+        self._batch_script_template = None
+        self.batch_script_template = batch_script_template
+
+    @property
+    def structure_file(self):
+        return self._structure_file
+
+    @structure_file.setter
+    def structure_file(self, structure_file):
+        if not structure_file:
+            error_message = '`structure_file` must be provided'
+            raise VaspRunManagerError(error_message)
+        self._structure = io.read_poscar(poscar_file=structure_file)
+        self._structure_file = structure_file
+
+    @property
+    def structure(self):
+        return self._structure
+
+    @property
+    def calculation_workflow(self):
+        return self._calculation_workflow
+
+    @calculation_workflow.setter
+    def calculation_workflow(self, calculation_workflow):
+        if not calculation_workflow:
+            self._calculation_workflow = 'relaxation'
+            return
+        if calculation_workflow.lower() not in ['relaxation', 'static']:
+            raise NotImplementedError('Only relaxation and static workflows currently implemented.')
+        self._calculation_workflow = calculation_workflow.lower()
+
+    @property
+    def run_location(self):
+        return self._run_location
+
+    @run_location.setter
+    def run_location(self, run_location):
+        if not run_location:
+            self._run_location = os.path.dirname(self.structure_file)
         else:
-            self.run_location = run_directory
+            self._run_location = run_location
 
-        #: compute resource where the calculations are being run: cori/edison/quest/...
-        self.host_resource = host_resource
+    @property
+    def host_scheduler_settings(self):
+        return self._host_scheduler_settings
 
-        #: batch scheduler on the compute resource: slurm/PBS/...
-        self.batch_scheduler = batch_scheduler
+    @host_scheduler_settings.setter
+    def host_scheduler_settings(self, host_scheduler_settings):
+        if not host_scheduler_settings:
+            self._host_scheduler_settings = DEFAULT_SCHEDULER_SETTINGS['cori_knl']
+            return
+        if os.path.isfile(host_scheduler_settings):
+            with open(host_scheduler_settings, 'r') as fr:
+                self._host_scheduler_settings = json.load(fr)
+        else:
+            settings = DEFAULT_SCHEDULER_SETTINGS.get(host_scheduler_settings)
+            if not settings:
+                error_message = 'Unable to find settings corresponding to the specified `host_scheduler_settings` tag'
+                raise VaspRunManagerError(error_message)
+            self._host_scheduler_settings = settings
 
-        #: dictionary of settings to go into the batch script.
-        #: Will be updated with `nondefault_scheduler_settings` if provided during object creation.
-        self.batch_scheduler_settings = DEFAULT_BATCH_SCHEDULER_SETTINGS[self.batch_scheduler][self.host_resource]
-        if nondefault_scheduler_settings is not None:
-            self.batch_scheduler_settings.update(nondefault_scheduler_settings)
+    @property
+    def batch_script_template(self):
+        return self._batch_script_template
+
+    @batch_script_template.setter
+    def batch_script_template(self, batch_script_template):
+        if not batch_script_template:
+            self._batch_script_template = 'cori.q'
+            return
+        if os.path.isfile(batch_script_template):
+            self._batch_script_template = batch_script_template
+        else:
+            template = SCHEDULER_TEMPLATES.get(batch_script_template)
+            if not template:
+                error_message = 'Unable to find template corresponding to the specified `batch_script_template` tag'
+                raise VaspRunManagerError(error_message)
+            self._batch_script_template = template
+
+    def _get_batch_script(self):
+        settings = {**self.host_scheduler_settings}
+        settings.update(**self.custom_scheduler_settings)
+        with open(self.batch_script_template, 'r') as fr:
+            template = fr.read()
+        template = template.format(**settings)
+        load_modules = []
+        for load_module in settings.get('modules', []):
+            load_modules.append('module load {}'.format(load_module))
+        template += '\n'.join(load_modules)
+        return template
+
+    @property
+    def batch_script(self):
+        return self._get_batch_script()
 
     def single_vasp_run(self, structure, location, calc_type='relaxation'):
         """Run a single VASP calculation of type `calc_type`.
@@ -144,7 +266,7 @@ class VaspRunManager(object):
 
 def generate_VASP_input():
 
-def generate_job_script():
+def generate_batch_script():
 
 def submit_job():
 
