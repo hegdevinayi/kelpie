2d305144c0be87c57247e44e86e1aa9e555e79c7
Vinay Hegde <hegdevinayi@gmail.com>
2018-01-03 21:13:39 -0600

    Add python-2 to setup classifiers

--- setup.py
+++ setup.py
@@ -20,6 +20,8 @@ setup(
     license='LICENSE',
     classifiers=[
         'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
     ],
