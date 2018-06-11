aa8f2b29174f39e80479396cf48dd9c84c71d6e8
Vinay Hegde <hegdevinayi@gmail.com>
2018-06-11 14:47:55 -0500

    Add skeletal module to read density of states info from vasprun.xml

--- kelpie/dos.py
+++ kelpie/dos.py
@@ -1,5 +1,7 @@
 import os
-from kelpie.vasp_output_parser import VasprunXMLParser
+import numpy as np
+import kelpie.vasp_output_parser as vasp_output_parser
+
 
 class DOSError(Exception):
     """Base class to handle error(s) related to density of states."""
@@ -16,16 +18,18 @@ class DOS(object):
         :param doscar_file: String with the path to the DOSCAR file from the calculation.
         """
         self._vasprunxml_file = None
+        self._vxparser = None
         self.vasprunxml_file = vasprunxml_file
 
         self._doscar_file = None
         self.doscar_file = doscar_file
+        self._doscarparser = None
 
-        self.unshifted_fermi_energy = self.read_fermi_energy()
-        self.energies = self.read_energies()
-        self.total_dos = self.read_total_dos()
-        self.total_integrated_dos = self.read_total_integrated_dos()
-        self.projected_dos = self.read_projected_dos()
+        self._total_dos_data = self.get_total_dos_data()
+        self._fermi_energy = self.get_fermi_energy()
+        self._energies = self.get_energies()
+        self._total_dos = self.get_total_dos()
+        self._total_integrated_dos = self.get_total_integrated_dos()
 
     @property
     def vasprunxml_file(self):
@@ -36,9 +40,14 @@ class DOS(object):
         if vasprunxml_file:
             if os.path.isfile(vasprunxml_file):
                 self._vasprunxml_file = vasprunxml_file
+                self._vxparser = vasp_output_parser.VasprunXMLParser(vasprunxml_file=vasprunxml_file)
             else:
                 error_message = 'Cannot find the vasprun.xml file specified: {}'.format(vasprunxml_file)
                 raise DOSError(error_message)
+
+    @property
+    def vxparser(self):
+        return self._vxparser
 
     @property
     def doscar_file(self):
@@ -53,132 +62,163 @@ class DOS(object):
                 error_message = 'Cannot find the DOSCAR file specified: {}'.format(doscar_file)
                 raise DOSError(error_message)
 
-    def read_fermi_energy(self):
-        if self.vasprunxml_file is not None:
-            return self.read_fermi_energy_from_xml()
-        elif self.doscar_file is not None:
-            return self.read_fermi_energy_from_doscar()
+    @property
+    def total_dos_data(self):
+        return self._total_dos_data
 
-    def read_fermi_energy_from_xml(self):
-        vxp = VasprunXMLParser(vasprunxml_file=self.vasprunxml_file)
-        return vxp.read_fermi_energy()
-
-    def read_fermi_energy_from_doscar(self):
-        raise NotImplementedError
-
-    def read_energies(self):
-        if self.vasprunxml_file is not None:
-            return self.read_energies_from_xml()
-        elif self.doscar_file is not None:
-            return self.read_energies_from_doscar()
-
-    def read_energies_from_xml(self):
-        vxp = VasprunXMLParser(vasprunxml_file=self.vasprunxml_file)
-        dos_data = vxp.read_total_dos()
-        if dos_data is None:
+    def get_total_dos_data(self):
+        if self.vxparser is None:
             return
-        return np.array([gridpoint[0] - self.fermi_energy for gridpoint in dos_data['spin_1']])
+        return self.vxparser.read_total_density_of_states()
 
-    def read_energies_from_poscar(self):
+    @property
+    def fermi_energy(self):
+        return self._fermi_energy
+
+    def get_fermi_energy(self):
+        if self.vasprunxml_file is not None:
+            return self.get_fermi_energy_from_xml()
+        elif self.doscar_file is not None:
+            return self.get_fermi_energy_from_doscar()
+
+    def get_fermi_energy_from_xml(self):
+        return self.vxparser.read_fermi_energy()
+
+    def get_fermi_energy_from_doscar(self):
         raise NotImplementedError
 
-    def read_total_dos(self):
-        if self.vasprunxml_file is not None:
-            return self.read_total_dos_from_xml()
-        elif self.doscar_file is not None:
-            return self.read_total_dos_from_doscar()
+    @property
+    def energies(self):
+        return self._energies
 
-    def read_total_dos_from_xml(self):
-        vxp = VasprunXMLParser(vasprunxml_file=self.vasprunxml_file)
-        dos_data = vxp.read_total_dos()
-        if dos_data is None:
+    def get_energies(self):
+        if self.vasprunxml_file is not None:
+            return self.get_energies_from_xml()
+        elif self.doscar_file is not None:
+            return self.get_energies_from_doscar()
+
+    def get_energies_from_xml(self):
+        if self.total_dos_data is None:
+            return
+        return np.array([gridpoint[0] - self.fermi_energy for gridpoint in self.total_dos_data['spin_1']])
+
+    def get_energies_from_doscar(self):
+        raise NotImplementedError
+
+    @property
+    def total_dos(self):
+        return self._total_dos
+
+    def get_total_dos(self):
+        if self.vasprunxml_file is not None:
+            return self.get_total_dos_from_xml()
+        elif self.doscar_file is not None:
+            return self.get_total_dos_from_doscar()
+
+    def get_total_dos_from_xml(self):
+        if not self.total_dos_data:
             return
         total_dos = {}
-        for spin in dos_data:
-            total_dos[spin] = np.array([gridpoint[1] for gridpoint in dos_data[spin]])
+        for spin in self.total_dos_data:
+            total_dos[spin] = np.array([gridpoint[1] for gridpoint in self.total_dos_data[spin]])
         return total_dos
 
-    def read_total_dos_from_doscar(self):
+    def get_total_dos_from_doscar(self):
         raise NotImplementedError
 
-    def read_total_integrated_dos(self):
-        if self.vasprunxml_file is not None:
-            return self.read_total_integrated_dos_from_xml()
-        elif self.doscar_file is not None:
-            return self.read_total_integrated_dos_from_doscar()
+    @property
+    def total_integrated_dos(self):
+        return self._total_integrated_dos
 
-    def read_total_integrated_dos_from_xml(self):
-        vxp = VasprunXMLParser(vasprunxml_file=self.vasprunxml_file)
-        dos_data = vxp.read_total_dos()
-        if dos_data is None:
+    def get_total_integrated_dos(self):
+        if self.vasprunxml_file is not None:
+            return self.get_total_integrated_dos_from_xml()
+        elif self.doscar_file is not None:
+            return self.get_total_integrated_dos_from_doscar()
+
+    def get_total_integrated_dos_from_xml(self):
+        if self.total_dos_data is None:
             return
         total_intdos = {}
-        for spin in dos_data:
-            total_intdos[spin] = np.array([gridpoint[2] for gridpoint in dos_data[spin]])
+        for spin in self.total_dos_data:
+            total_intdos[spin] = np.array([gridpoint[2] for gridpoint in self.total_dos_data[spin]])
+        return total_intdos
 
-    def read_total_integrated_dos_from_doscar(self):
+    def get_total_integrated_dos_from_doscar(self):
         raise NotImplementedError
 
-    def read_projected_dos(self):
+    @property
+    def projected_dos(self):
+        return self._projected_dos
+
+    def get_projected_dos(self):
         if self.vasprunxml_file is not None:
-            return self.read_projected_dos_from_xml()
+            return self.get_projected_dos_from_xml()
         elif self.doscar_file is not None:
-            return self.read_projected_dos_from_doscar()
+            return self.get_projected_dos_from_doscar()
 
-    def read_projected_dos_from_xml(self):
+    def get_projected_dos_from_xml(self):
         raise NotImplementedError
 
-    def read_projected_dos_from_doscar(self):
+    def get_projected_dos_from_doscar(self):
         raise NotImplementedError
 
-    def is_metal(self, tol=1e-3):
-        if any([v is None for v in [self.total_dos, self.energies]]):
-            self.read_total_dos()
-            self.read_energies()
-            if any([v is None for v in [self.total_dos, self.energies]]):
-                error_message = 'Cannot read DOS data from file'
-                raise DOSError(error_message)
+    def _check_if_metal(self, spin='spin_1', tol=1e-3):
         efermi_index = np.abs(self.energies).argmin()
         if self.energies[efermi_index] < 0:
             bracket_index = efermi_index + 1
         elif self.energies[efermi_index] > 0:
             bracket_index = efermi_index - 1
-        return ((self.total_dos[efermi_index] > tol) and (self.total_dos[bracket_index] > tol))
+        return ((self.total_dos[spin][efermi_index] > tol) and (self.total_dos[spin][bracket_index] > tol))
 
-    def find_vbm(self, tol=1e-3):
-        if self.is_metal(tol=tol):
+    def check_if_metal(self, tol=1e-3):
+        return dict([(spin, self._check_if_metal(spin=spin, tol=tol)) for spin in self.total_dos])
+
+    @property
+    def is_metal(self):
+        return self.check_if_metal()
+
+    def _find_vbm(self, spin='spin_1', tol=1e-3):
+        if self._check_if_metal(spin=spin, tol=tol):
             return
         efermi_index = np.abs(self.energies).argmin()
         if self.energies[efermi_index] < 0:
             efermi_index += 1
         index = efermi_index
-        while self.total_dos[index] < tol:
+        while self.total_dos[spin][index] < tol:
             index -= 1
         return self.energies[index]
+
+    def find_vbm(self, tol=1e-3):
+        return dict([(spin, self._find_vbm(spin=spin, tol=tol)) for spin in self.total_dos])
 
     @property
     def vbm(self):
         return self.find_vbm()
 
-    def find_cbm(self, tol=1e-3):
-        if self.is_metal(tol=tol):
+    def _find_cbm(self, spin='spin_1', tol=1e-3):
+        if self._check_if_metal(spin=spin, tol=tol):
             return
         efermi_index = np.abs(self.energies).argmin()
         if self.energies[efermi_index] > 0:
             efermi_index -= 1
         index = efermi_index
-        while self.total_dos[index] < tol:
+        while self.total_dos[spin][index] < tol:
             index += 1
         return self.energies[index]
+
+    def find_cbm(self, tol=1e-3):
+        return dict([(spin, self._find_cbm(spin=spin, tol=tol)) for spin in self.total_dos])
 
     @property
     def cbm(self):
         return self.find_cbm()
 
+    def _calculate_band_gap(self, spin='spin_1', tol=1e-3):
+        return (self.cbm[spin] - self.vbm[spin]) if not self._check_if_metal(spin=spin, tol=tol) else 0.0
+
     def calculate_band_gap(self, tol=1e-3):
-        if self.is_metal(tol=tol):
-            return 0.0
-        return self.cbm - self.vbm
+        return dict([(spin, self._calculate_band_gap(spin=spin, tol=tol)) for spin in self.total_dos])
 
     @property
     def band_gap(self):
