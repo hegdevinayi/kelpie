7b8c1aab09c2ce99e10dccd6ebf5a96883754cdb
Vinay Hegde <hegdevinayi@gmail.com>
2018-06-11 15:07:23 -0500

    Add density of states properties to the calculation data container
    1. "vasprun.xml" is no longer the default value in the constructor.
    2. Delete all internal attributes, properties return functions instead
    3. The only attributes are vasprun.xml parser and DOS objects
    4. Add density of states related properties
    5. Refactor calculation_as_dict() -> as_dict()
    6. as_dict() now uses utils.serialization.jsonable() to convert
    non-standard datatypes into JSON-serializable ones

--- kelpie/vasp_calculation_data.py
+++ kelpie/vasp_calculation_data.py
@@ -1,43 +1,29 @@
-import os
-import json
 import numpy
-from kelpie.vasp_output_parser import VasprunXMLParser
+import kelpie.vasp_output_parser as vasp_output_parser
+import kelpie.dos as dos
+from kelpie.utils.serialization import jsonable
 
 
-class VaspCalculationError(Exception):
+class VaspCalculationDataError(Exception):
+    """Base class to handle errors in the VASP calculation data container."""
     pass
 
 
 class VaspCalculationData(object):
     """Base class to store output data from a VASP calculation."""
 
-    def __init__(self, vasprunxml_file='vasprun.xml'):
+    def __init__(self, vasprunxml_file=None):
         """
         :param vasprunxml_file: Path to the vasprun.xml file to parse.
         """
         self._vasprunxml_file = None
         self.vasprunxml_file = vasprunxml_file
 
-        self._vxparser = VasprunXMLParser(self.vasprunxml_file)
-        self._run_timestamp = self.vxparser.read_run_timestamp()
-        self._composition_info = self.vxparser.read_composition_information()
-        self._list_of_atoms = self.vxparser.read_list_of_atoms()
-        self._n_ionic_steps = self.vxparser.read_number_of_ionic_steps()
-        self._scf_energies = self.vxparser.read_scf_energies()
-        self._entropies = self.vxparser.read_entropies()
-        self._free_energies = self.vxparser.read_free_energies()
-        self._forces = self.vxparser.read_forces()
-        self._stress_tensors = self.vxparser.read_stress_tensors()
-        self._lattice_vectors = self.vxparser.read_lattice_vectors()
-        self._atomic_coordinates = self.vxparser.read_atomic_coordinates()
-        self._cell_volumes = self.vxparser.read_cell_volumes()
-        self._kpoint_mesh = self.vxparser.read_kpoint_mesh()
-        self._irreducible_kpoints = self.vxparser.read_number_of_irreducible_kpoints()
-        self._fermi_energy = self.vxparser.read_fermi_energy()
-        self._total_dos = self.vxparser.read_total_dos()
-        self._band_occupations = self.vxparser.read_band_occupations()
-        self._scf_looptimes = self.vxparser.read_scf_looptimes()
-        self._total_runtime = self._calculate_total_runtime(self.scf_looptimes)
+        # Data from vasp_output_parser.VasprunXMLParser accessible as properties
+        self._vxparser = vasp_output_parser.VasprunXMLParser(vasprunxml_file=self.vasprunxml_file)
+
+        # Data from dos.DOS accessible as properties
+        self._density_of_states = dos.DOS(vasprunxml_file=self.vasprunxml_file)
 
     @property
     def vasprunxml_file(self):
@@ -45,11 +31,7 @@ class VaspCalculationData(object):
 
     @vasprunxml_file.setter
     def vasprunxml_file(self, vasprunxml_file):
-        if os.path.isfile(vasprunxml_file):
-            self._vasprunxml_file = vasprunxml_file
-        else:
-            error_msg = 'VASP output file {} not found'.format(vasprunxml_file)
-            raise VaspCalculationError(error_msg)
+        self._vasprunxml_file = vasprunxml_file
 
     @property
     def vxparser(self):
@@ -61,59 +43,65 @@ class VaspCalculationData(object):
 
     @property
     def run_timestamp(self):
-        return self._run_timestamp
+        return self.vxparser.read_run_timestamp()
 
     @property
     def composition_info(self):
-        return self._composition_info
+        return self.vxparser.read_composition_information()
 
     @property
     def list_of_atoms(self):
-        return self._list_of_atoms
+        return self.vxparser.read_list_of_atoms()
+
+    @property
+    def n_atoms(self):
+        if not self.list_of_atoms:
+            return
+        return len(self.list_of_atoms)
 
     @property
     def n_ionic_steps(self):
-        return self._n_ionic_steps
+        return self.vxparser.read_number_of_ionic_steps()
 
     @property
     def scf_energies(self):
-        return self._scf_energies
+        return self.vxparser.read_scf_energies()
 
     @property
     def entropies(self):
-        return self._entropies
+        return self.vxparser.read_entropies()
 
     @property
     def free_energies(self):
-        return self._free_energies
+        return self.vxparser.read_free_energies()
 
     @property
     def forces(self):
-        return self._forces
+        return self.vxparser.read_forces()
 
     @property
     def stress_tensors(self):
-        return self._stress_tensors
+        return self.vxparser.read_stress_tensors()
 
     @property
     def lattice_vectors(self):
-        return self._lattice_vectors
+        return self.vxparser.read_lattice_vectors()
 
     @property
     def atomic_coordinates(self):
-        return self._atomic_coordinates
+        return self.vxparser.read_atomic_coordinates()
 
     @property
     def cell_volumes(self):
-        return self._cell_volumes
+        return self.vxparser.read_cell_volumes()
 
     @property
     def kpoint_mesh(self):
-        return self._kpoint_mesh
+        return self.vxparser.read_kpoint_mesh()
 
     @property
     def irreducible_kpoints(self):
-        return self._irreducible_kpoints
+        return self.vxparser.read_irreducible_kpoints()
 
     @property
     def n_irreducible_kpoints(self):
@@ -122,16 +110,8 @@ class VaspCalculationData(object):
         return len(self.irreducible_kpoints)
 
     @property
-    def fermi_energy(self):
-        return self._fermi_energy
-
-    @property
-    def total_dos(self):
-        return self._total_dos
-
-    @property
     def band_occupations(self):
-        return self._band_occupations
+        return self.vxparser.read_band_occupations()
 
     @property
     def nbands(self):
@@ -141,26 +121,114 @@ class VaspCalculationData(object):
 
     @property
     def scf_looptimes(self):
-        return self._scf_looptimes
+        return self.vxparser.read_scf_looptimes()
 
     @property
     def total_runtime(self):
-        return self._total_runtime
+        return self._calculate_total_runtime()
 
-    @staticmethod
-    def _calculate_total_runtime(scf_looptimes):
+    def _calculate_total_runtime(self):
         """Sum up all SCF looptimes to calculate the total runtime in seconds.
 
-        :param scf_looptimes: loop times for each SCF in every ionic step.
-                              - see `VasprunXMLParser.read_scf_looptimes()`
-        :type scf_looptimes: dict(int, list(float))
         :return: total runtime for the calculation in seconds.
         :rtype: float
         """
         total_runtime = 0.
-        for n_ionic_step, scstep_looptimes in scf_looptimes.items():
+        for n_ionic_step, scstep_looptimes in self.scf_looptimes.items():
             total_runtime += sum(scstep_looptimes)
         return total_runtime
+
+    @property
+    def density_of_states(self):
+        return self._density_of_states
+
+    @property
+    def fermi_energy(self):
+        return self.density_of_states.fermi_energy
+
+    @property
+    def dos_energy_grid(self):
+        return self.density_of_states.energies
+
+    @property
+    def total_dos(self):
+        return self.density_of_states.total_dos
+
+    @property
+    def total_integrated_dos(self):
+        return self.density_of_states.total_integrated_dos
+
+    @property
+    def is_metal(self):
+        return self.density_of_states.is_metal
+
+    @property
+    def valence_band_maximum(self):
+        return self.density_of_states.vbm
+
+    @property
+    def conduction_band_minimum(self):
+        return self.density_of_states.cbm
+
+    @property
+    def dos_band_gap(self):
+        return self.density_of_states.band_gap
+
+    @property
+    def initial_entropy(self):
+        return self.entropies.get(0, None)
+
+    @property
+    def final_entropy(self):
+        return self.entropies.get(self.n_ionic_steps - 1, None)
+
+    @property
+    def initial_free_energy(self):
+        return self.free_energies.get(0, None)
+
+    @property
+    def final_free_energy(self):
+        return self.free_energies.get(self.n_ionic_steps - 1, None)
+
+    @property
+    def intial_forces(self):
+        return self.forces.get(0, None)
+
+    @property
+    def final_forces(self):
+        return self.forces.get(self.n_ionic_steps - 1, None)
+
+    @property
+    def initial_stress_tensor(self):
+        return self.stress_tensors.get(0, None)
+
+    @property
+    def final_stress_tensor(self):
+        return self.stress_tensors(self.n_ionic_steps - 1, None)
+
+    @property
+    def initial_cell_volume(self):
+        return self.cell_volumes.get(0, None)
+
+    @property
+    def final_cell_volume(self):
+        return self.cell_volumes.get(self.n_ionic_steps - 1, None)
+
+    @property
+    def initial_lattice_vectors(self):
+        return self.lattice_vectors.get(0, None)
+
+    @property
+    def final_lattice_vectors(self):
+        return self.lattice_vectors(self.n_ionic_steps - 1, None)
+
+    @property
+    def initial_atomic_coordinates(self):
+        return self.atomic_coordinates.get(0, None)
+
+    @property
+    def final_atomic_coordinates(self):
+        return self.atomic_coordinates.get(self.n_ionic_steps - 1, None)
 
     def is_scf_converged(self, threshold=1E-6, each_ionic_step=False):
         if not each_ionic_step:
@@ -225,32 +293,57 @@ class VaspCalculationData(object):
                      self.is_number_of_bands_converged(threshold=band_occ_thresh))
         return converged
 
-    @property
-    def calculation_data_as_dict(self):
-        calculation_data = {
-            'run_timestamp': self.timestamp_to_str(self.run_timestamp),
-            'composition_info': self.composition_info,
-            'list_of_atoms': self.list_of_atoms,
-            'n_ionic_steps': self.n_ionic_steps,
-            'free_energies': self.free_energies,
-            'forces': self.forces[self.n_ionic_steps - 1].tolist(),
-            'initial_stress': self.stress_tensors[0].tolist(),
-            'final_stress': self.stress_tensors[self.n_ionic_steps - 1].tolist(),
-            'initial_lattice_vectors': self.lattice_vectors[0].tolist(),
-            'final_lattice_vectors': self.lattice_vectors[self.n_ionic_steps - 1].tolist(),
-            'initial_volume': self.cell_volumes[0],
-            'final_volume': self.cell_volumes[self.n_ionic_steps - 1],
-            'fermi_energy': self.fermi_energy,
-            'total_runtime': self.total_runtime,
-            'scf_converged': self.is_scf_converged(),
-            'forces_converged': self.are_forces_converged(),
-            'bands_converged': self.is_number_of_bands_converged(),
-            'basis_converged': self.is_basis_converged(),
-            'fully_converged': self.is_fully_converged()
-        }
-        return calculation_data
+    def as_dict(self, list_of_properties=None):
+        _PROPERTIES = [
+            'run_timestamp',
+            'composition_info',
+            'list_of_atoms',
+            'n_atoms',
+            'n_ionic_steps',
+            'scf_energies',
+            'entropies',
+            'free_energies',
+            'forces',
+            'stress_tensors',
+            'lattice_vectors',
+            'atomic_coordinates',
+            'cell_volumes',
+            'kpoint_mesh',
+            'irreducible_kpoints',
+            'n_irreducible_kpoints',
+            'band_occupations',
+            'n_bands',
+            'scf_looptimes',
+            'total_runtime',
+            'fermi_energy',
+            'dos_energy_grid',
+            'total_dos',
+            'total_integrated_dos',
+            'is_metal',
+            'valence_band_maximum',
+            'conduction_band_minimum',
+            'dos_band_gap',
+            'initial_entropy',
+            'final_entropy',
+            'initial_free_energy',
+            'final_free_energy',
+            'initial_forces',
+            'final_forces',
+            'initial_stress_tensor',
+            'final_stress_tensor',
+            'initial_cell_volume',
+            'final_cell_volume',
+            'initial_lattice_vectors',
+            'final_lattice_vectors',
+            'initial_atomic_coordinates',
+            'final_atomic_coordinates',
+            'is_scf_converged',
+            'are_forces_converged',
+            'is_number_of_bands_converged',
+            'is_basis_converged',
+            'is_fully_converged',
+        ]
 
-    def write_calculation_data(self, filename='calculation_data.json'):
-        with open(filename, 'w') as fw:
-            json.dump(self.calculation_data_as_dict, fw, indent=2)
-
+        if list_of_properties is not None:
+            _PROPERTIES = list_of_properties
+        return dict([(prop, jsonable(getattr(self, prop, None))) for prop in _PROPERTIES])
